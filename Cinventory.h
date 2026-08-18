29b4c549efd5be8a9e4eda34fd2b4afd206bebbc
Hairil Iqwan <251312G@mymail.nyp.edu.sg>
2026-08-18 14:41:10 +0800

    Added inventory system

---
+++ Cinventory.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
+
+class inventorySystem
+{
+	string inventorySpace[10] = {"A","B","C","D","y"};
+	int maxItem = 10;
+	int itemPosition = 0;
+	bool isInventoryOpen = false;
+public:
+	void renderInventory();
+	void showInventory(char input);
+	void addToInventory(string item);
+	void switchItem(int input);
+	bool getInventoryState();
+};
+
