a8b188e73b4e343d2eafeeebc7c4cf78319705b1
noctagnosis <mohamedilhanj@gmail.com>
2026-08-18 12:02:28 +0800

    Change map and player pointer edits
    the map change function has been added as well as changing the player pointers

--- CGameManager.cpp
+++ CGameManager.cpp
@@ -1,9 +1,40 @@
 #include "CGameManager.h"
 
-CGameManager::CGameManager() {
-	map.SetRoom(25,10);
-	for (int i = 0; i < 100; i++) {
-		map.Movement();
+void CGameManager::SetMaps() {
+    map[0].SetRoom(10, 6);
+    map[1].SetRoom(6, 10);
+}
+//KAI XIN change this part for the change map mechanic
+void CGameManager::changeMaps(char input)
+{
+    if (input == 'f') {
+        map[currentMap].removePosition(
+            map[currentMap].GetPlayer()->GetPosY(),
+            map[currentMap].GetPlayer()->GetPosX()
+        );
+        if (currentMap == 0) {
+            currentMap = 1;
+        }
+        else {
+            currentMap = 0;
+        }
+        map[currentMap].GetPlayer()->SetPosX(0);
+        map[currentMap].GetPlayer()->SetPosY(0);
+        map[currentMap].SetPosition();
+        map[currentMap].RenderMap();
+    }
+    else {
+        map[currentMap].Movement(input);
+    }
+}
 
-	}
+CGameManager::CGameManager() {
+    currentMap = 0;
+    SetMaps();
+    map[currentMap].RenderMap();
+
+    for (int i = 0; i < 100; i++) {
+        char input = _getch();
+        changeMaps(input);
+    }
 }
