0dc4be3c1804ad4fc393f06dbc4647d3f1125bf1
UncleIan <ianchaierung@gmail.com>
2026-08-24 15:26:15 +0800

    added getCurrentNode

--- CNPC.h
+++ CNPC.h
@@ -65,6 +65,7 @@ public:
 	bool getKillerStatus();
 	string getDialogue();
 	string setDialogue(string text);
+	int getCurrentNode();//for items
 
 	void ResetDialogueTree();
 	void RenderDialougeSystem(bool typetext, CMap* map);
